3405038f39e012a89247cd73b37d84befc4560e7
nymishkash <reachnymish@gmail.com>
2024-06-24 11:02:43 +0530

    addtocart functionality update

--- src/store.js
+++ src/store.js
@@ -2,6 +2,30 @@ import { createStore } from "redux";
 function cartReducer(state= { items: {}}, action) {
   switch (action.type) {
     case "ADD_TO_CART": {
+      const product = action.payload;
+      if (state.items[product.id]) {
+        return {
+          ...state,
+          items:{
+            ...state.items,
+            [product.id]: {
+              ...state.items[product.id],
+              quantity: state.items[product.id].quantity + 1
+            }
+          }
+        }
+      } else {
+        return {
+          ...state,
+          items: {
+            ...state.items,
+            [product.id]: {
+              ...product,
+              quantity: 1
+            }
+          }
+        }
+      }
 
     }
     case "REMOVE_FROM_CART": {
